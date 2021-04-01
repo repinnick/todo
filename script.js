0eafc91f6310472d3b13667ad7257118a95fd053
igotbitches <check.point9917@yandex.ru>
2021-04-01 22:02:19 +0300

    add some funcrions

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 const $currentTasks = document.querySelector("#currentTasks");
 const $priority = document.querySelectorAll(".form-check-input");
 const $btnAddTask = document.querySelector("#add-button");
+//
+const items = $currentTasks.querySelectorAll("li");
+items.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    console.log(event.target);
+  });
+});
 
-// get title, text, priority
+// const item = $currentTasks.children;
+// console.log(Array.from(item));
+// Array.from(item).forEach((element) => {
+//   element.addEventListener("click", (event) => {
+//     console.log(event.target);
+//   });
+// });
+
+/* Experimental
+const $modalBody = document.querySelector("#exampleModal");
+const $body = document.body;
+
+function showModal() {
+  const innerItem = `<div class="modal-backdrop fade show"></div>`;
+  $body.classList.add("modal-open");
+  $modalBody.classList.add("show");
+  $modalBody.style.display = "block";
+  document.body.insertAdjacentHTML("beforeend", innerItem);
+}
+
+function closeModal() {
+  let innerItem = document.querySelector(".modal-backdrop");
+  innerItem = "";
+  $body.classList.remove("modal-open");
+  $modalBody.classList.remove("show");
+  $modalBody.style.display = "none";
+}
+*/
+
+// add task after click
 $btnAddTask.addEventListener("click", (event) => {
   event.preventDefault();
-  const title = document.querySelector("#inputTitle").value;
-  const text = document.querySelector("#inputText").value;
-  const priorityItems = document.querySelectorAll(".form-check-input");
-  const date = getDate();
-  let priority = "";
-
-  for (let item of priorityItems) {
-    if (item.checked) {
-      priority = item.value;
-    }
-  }
-  addTask(title, text, priority, date);
+  const getData = createTask();
+  addTask(getData.title, getData.text, getData.priority, getData.date);
   document.querySelector("form").reset();
 });
 
@@ -35,11 +61,11 @@ function addTask(title, text, priority, date) {
                     <p class="mb-1 w-100">${text}</p>
                 </div>
                 <div class="dropdown m-2 dropleft">
-                    <button class="btn btn-secondary h-100" type="button" id="dropdownMenuItem1" 
+                    <button class="btn btn-secondary h-100" type="button" 
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fas fa-ellipsis-v"></i>
                     </button>
-                    <div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1">
+                    <div class="dropdown-menu p-2 flex-column">
                         <button type="button" class="btn btn-success w-100">Complete</button>
                         <button type="button" class="btn btn-info w-100 my-2">Edit</button>
                         <button type="button" class="btn btn-danger w-100">Delete</button>
@@ -50,7 +76,29 @@ function addTask(title, text, priority, date) {
   $currentTasks.insertAdjacentHTML("afterbegin", taskItem);
 }
 
-function createTask() {}
+// get title, text, priority, date
+function createTask() {
+  const title = document.querySelector("#inputTitle").value;
+  const text = document.querySelector("#inputText").value;
+  const priorityItems = document.querySelectorAll(".form-check-input");
+  const date = getDate();
+  let priority = "";
+
+  for (let item of priorityItems) {
+    if (item.checked) {
+      priority = item.value;
+    }
+  }
+
+  let obj = {
+    title: title,
+    text: text,
+    priority: priority,
+    date: date,
+  };
+
+  return obj;
+}
 
 // date create
 function getDate() {
